df641e188f33be00b018055d297d15d1501bbad6
João Oliveira <contato.jpshawty@gmail.com>
2025-02-17 23:39:49 -0300

    Add files via upload

---
+++ programação_web_2/aula-01-web-server/app.js
@@ -0,0 +1,10 @@
+// criando a constante express
+// fazendo o require, importando a biblioteca
+// buscando a biblioteca
+const express = require('express');
+
+// chamando o metedo construidor, o express
+const app = express();
+
+// exportando para a chamada, podendo agora ser usado
+module.exports = app;
